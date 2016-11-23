39ba3fbc2987980108ca0c980958a65e9290a44b
Alex Paz <alexjpaz@gmail.com>
2016-11-23 13:23:38 -0500

    1

--- upload.sh
+++ upload.sh
@@ -2,7 +2,7 @@ docker run --rm \
   -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
   -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
   -e AWS_DEFAULT_REGION=us-east-1 \
-  -v $PWD:/app xueshanf/awscli\
+  -v $PWD:/app xueshanf/awscli \
     aws s3api put-bucket-website \
-    --bucket pazthe.ninja
+    --bucket pazthe.ninja \
     --website-configuration file://website.json
